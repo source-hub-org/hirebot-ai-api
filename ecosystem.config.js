9a72b9c6af5be551a46c83c68aac5347e5488d4c
tranthethang <tranthethang@gmail.com>
2025-05-09 11:33:36 +0700

    update config of docker to sync source code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,19 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      ignore_watch: ['node_modules', 'logs'],
       max_memory_restart: '1G',
-      env: {
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+        MONGODB_URI: 'mongodb://mongodb:27017',
+        DB_NAME: 'hirebot_db',
+        REDIS_HOST: 'redis',
+        REDIS_PORT: 6379,
+        JOB_POLLING_INTERVAL: 5000,
+        watch: true,
+      },
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3000,
         MONGODB_URI: 'mongodb://mongodb:27017',
@@ -15,6 +26,7 @@ module.exports = {
         REDIS_HOST: 'redis',
         REDIS_PORT: 6379,
         JOB_POLLING_INTERVAL: 5000,
+        watch: false,
       },
     },
   ],
